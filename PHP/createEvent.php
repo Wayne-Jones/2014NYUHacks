9cb27c8e403c6bcb4e0729ba08e10377967e4afd
Martin <mr.martinsosa@gmail.com>
2014-05-02 22:07:24 -0400

    File Updates
    Deleted test files

--- PHP/createEvent.php
+++ PHP/createEvent.php
@@ -4,26 +4,27 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>I'm Touching The Folder</title>
+    <title>Dashboard- Create an Event!</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/nocsshere.css">
       
-
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+      <link rel="stylesheet" type="text/css" href="maps.css">
+        <script type="text/javascript" src="js/maps.js"></script>
   </head>
+    <body style="background-image:url(images/food.png)">
 <nav class="navbar navbar-default" role="navigation">
   <div class="container-fluid">
     <!-- Title and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       
-      <a class="navbar-brand" href="#">NYUSquare</a>
+      <a class="navbar-brand" href="#">Happy Trail</a>
       <a class="navbar-brand" href="#">Log in</a>
       <!--<p class="navbar-text">Signed in as user</p>!-->
     </div>
@@ -36,26 +37,37 @@
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
-    <h1>Test hi Dawid </h1>
-<br></br><br></br><br></br><br></br>
+
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">
+    Insert list of bars here
+  </div>
+    </div>
+<br></br>
 <div class="wrapper">
 <div class="row">
-    <div class="span4" style="text-align:center">
-    <div class="col-md-4"></div>
-    <div class="col-md-4">
-    <button class="btn btn-xlarge" class="btn btn-default navbar-btn">MAGIC BUTTON </button> </button>
-    <div class="col-md-4"></div>
-    </div>
-    </div>
+    
 </div>
 <br></br>
-<div class="row">
-    <div class="span4" style="text-align:center">
+
+<div class="panel panel-default">
+  <div class="panel-body">
+    <div>
+            <input id="autocomplete" placeholder="Enter your address" type="text"></input>
+            </pre><div id="map_canvas" style="height:500px"></div><pre></pre>
+        </div>
+  </div>
+    </div>
+
+<div class="span4" style="text-align:center">
     <div class="col-md-4"></div>
     <div class="col-md-4">
+    <button class="btn btn-xlarge" class="btn btn-default navbar-btn">Bar Hop! </button> </button>
     <div class="col-md-4"></div>
-        </div>
-</div>
+    </div>
+    </div>
+
 <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
