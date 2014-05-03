72cd1c329bc988c3c2ab7f650965db89423cecd2
Martin <mr.martinsosa@gmail.com>
2014-05-03 15:17:05 -0400

    Centered map to Washington Square Park

--- maps.php
+++ maps.php
@@ -13,8 +13,8 @@
     <script type="text/javascript">
       function initialize() {
         var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8
+          center: new google.maps.LatLng(40.7308, -73.9975),
+          zoom: 17
         };
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
